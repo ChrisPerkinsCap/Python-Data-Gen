e6d2233cc1dfdefcb1bbd00cb9ffbf3bd64acd09
CPERKINS <christopher.perkins@capgemini.com>
2022-11-16 14:01:00 +0100

    Adjusted GeneratedRandomFirstNamesCSV for new input file

--- test.py
+++ test.py
@@ -1,15 +1,50 @@
 from GenerateRandomFirstNamesCSV import GenerateRandomFirstNamesCSV
+import copy
+from datetime import datetime
 
 source_dir = "./CSV/"
-source_file = "FirstNames-fr.csv"
-target_file = "FirstNames-fr-Popularité.csv"
-drop_column = "Random Value"
-sort_column = "Popularité"
+source_file = "FirstNames-Popularity-fr.csv"
+target_file = "FirstNames-Popularity-fr.csv"
+drop_columns = ["NameCount", "Rand"]
+# sort_columns = ["name","sex","NameCount", "Rand"]
+sort_columns = ["Popularity"]
+final_sort = ["FirstName", "Gender", "Popularity"]
+columns_order = ["Popularity", "Gender", "FirstName"]
+# drop_columns = ["Random Value"]
+# sort_columns = ["Prénom", "sexe", "Popularité", "Random Value"]
+# columns_order = ["Popularité", "sexe", "Prénom", "Random Value"]
 num_rows = 172
 
-RG = GenerateRandomFirstNamesCSV(num_rows, source_dir, source_file, target_file)
-
-df = RG.process_csv_file(source_dir, source_file, num_rows, drop_column, sort_column)
+RG = GenerateRandomFirstNamesCSV(columns_order, drop_columns, final_sort, num_rows,
+                                 sort_columns, source_dir, source_file)
+# print(len(drop_columns))
+# df = RG.process_csv_file(columns_order, drop_columns, final_sort, num_rows,
+#                          sort_columns, source_dir, source_file)
 
 RG.print_data_frame()
 
+# data = GenerateRandomFirstNamesCSV.process_csv_file(columns_order, drop_columns,
+#                                     final_sort, num_rows, sort_columns, source_dir, source_file)
+
+# print(data.describe())
+# print(data)
+
+# fresh_data = copy.copy(data.reindex())
+
+# print(fresh_data)
+
+fresh_data = RG.remove_column("Rand")
+
+fresh_data.rename(columns={'name':'FirstName', 'sex':'Gender', 'NameCount':'Popularity'}, inplace=True)
+
+RG.print_data_frame()
+
+print(RG.get_data())
+
+now_time = datetime.now()
+now_time_ft = now_time.strftime("%d-%m-%Y_%H:%M:%S")
+
+print(now_time_ft)
+
+# GenerateRandomFirstNamesCSV.write_to_csv(
+#     fresh_data, source_dir, "FirstNames-Popularity-fr.csv")
